e5c3ac7a26279f9bd86f9e7c4a994b50ff5db4fd
XYZTech <10484324+brandy2015@users.noreply.github.com>
2023-04-26 10:48:41 +0800

    0.0.9
    0.0.9

--- XYZMapKit/XYZLocationKit/XYZMKPointAnnotation.swift
+++ XYZMapKit/XYZLocationKit/XYZMKPointAnnotation.swift
@@ -17,7 +17,25 @@ class XYZMKPointAnnotation{
         
         self.objectAnnotation = objectAnnotation
     }
+    
+    
+    var coordinate: CLLocationCoordinate2D!
+   
+    var cllocation:CLLocation!{
+        return coordinate.CLLocationx
+    }
+    init(placeMark:CLPlacemark){
+        guard let coordinate = placeMark.location?.coordinate else{return}
         
+        self.coordinate = coordinate
+        let objectAnnotation = MKPointAnnotation()
+        objectAnnotation.coordinate = coordinate//coordinate//设置注释对象的地理位置
+        
+        objectAnnotation.title      = placeMark.ChinesShortPlaceAddress//设置注释对象的标题内容
+        objectAnnotation.subtitle   = placeMark.ChinesPlaceAddress //设置注释对象的字标题内容
+        
+        self.objectAnnotation = objectAnnotation
+    }
 }
 
 public extension MKMapView{
